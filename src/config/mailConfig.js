c2ec1e4bb1d3330454db0fe7a9c776388a758217
Abhinav Sinha <asinha093@gmail.com>
2021-05-06 19:18:02 +0530

    rermove href from subject line

--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -1,7 +1,7 @@
 const SERVICE_PROVIDER = 'Gmail';
 const RECIPIENT = 'asinha093@gmail.com';
 const SENDER = 'covid-19-vaccine-alerts-cowin';
-const SUBJECT = 'New vaccination slots are available on https://www.cowin.gov.in Book appointment now';
+const SUBJECT = 'New vaccination slots are available on COWIN. Book appointment now';
 const BODY = 'There are now new COVID-19 vaccination slots available in your requested location(s)';
 
 module.exports = { SERVICE_PROVIDER, RECIPIENT, SENDER, SUBJECT, BODY };
