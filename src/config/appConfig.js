23cb0c9f34129cee5e2bf82594839a7d68b084c3
Abhinav Sinha <asinha093@gmail.com>
2021-05-07 15:56:48 +0530

    add sample alert image to project

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,10 +1,10 @@
 const FINDBYPINCODE = true;
 // location configs
-const PINCODE = '800001,800002,800012,800013';
+const PINCODE = '800001';
 const STATE = 'BIHAR';
 const DISTRICT = 'PATNA';
 // beneficiary configs
 const EMAIL = 'asinha093@gmail.com';
-const AGE = 28;
+const AGE = 45;
 
 module.exports = { FINDBYPINCODE, PINCODE, STATE, DISTRICT, EMAIL, AGE };
